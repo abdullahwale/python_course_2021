d3c4d19ec1a68c03b9af5edb46b22b15b6807678
abdullahwale <abdullahwale@gmail.com>
2020-12-28 10:46:18 +0500

    class 04 code for fixing errros

---
+++ fixingerrors.py
@@ -0,0 +1,6 @@
+#Fix the code below with have different errors
+
+print(Week 01 - String Manipulation")
+print("String Concatenation is done with the "+" sign.")
+  print('e.g. print("Hello " + "world")')
+print(("New lines can be created with a backslash and n.")
