afe0142cbf93dd912bd7b062174ee75f0c30e818
abdullahwale <abdullahwale@gmail.com>
2021-01-06 13:46:58 +0500

    profile genrator nad primitive datatypes

---
+++ Profile Genrator Project.py
@@ -0,0 +1,12 @@
+# Quiz Link: https://realpython.com/quizzes/python-variables/
+#Profile Genrator Project on Variable
+name=input("Enter Your Full Name")
+addres=input("Enter your Address")
+phone=input("Enter your Phone Number")
+########################################
+print("\n\nYour Complete Prfile")
+print("====================")
+print(name)
+print(addres)
+print(phone)
+print("====================")
---
+++ primitive_datatypes.py
@@ -0,0 +1,19 @@
+#Data Types
+#Four String, Integer, Float,  Boolean //Primitive Data
+
+#String
+#print(12 + 113)
+print("Muhammad"[1])
+
+#Integer
+#123,346,579
+print(123_346_579)
+
+#Float
+print(234.56)
+
+#Boolean
+#True & False
+print(False)
+
+
