da650c3c6238f347ace82c457db7b5073e2dc6b1
abdullahwale <abdullahwale@gmail.com>
2021-01-06 14:16:51 +0500

    Type Error, Type KeyWord and Type Conversion Functions

---
+++ type conversion_type_error.py
@@ -0,0 +1,6 @@
+print("Type Error, Type KeyWord and Type Conversion Functions")
+name=len(input("Enter your name"))
+print(type(name))
+conver_value=str(name)
+print("Your Name have " + conver_value + " Character's")
+
