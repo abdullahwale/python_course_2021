d2a93c2ebbcdd5610cea8dcfd80197952691c5f3
abdullahwale <abdullahwale@gmail.com>
2021-01-07 07:19:31 +0500

    DataType Coding Practice
    Write a progrma that take two digit number from the user and display its sum and in display in reverse order.

---
+++ datatype coding practice.py
@@ -0,0 +1,13 @@
+# Write a program that take two digit #number from the user
+# and display its sum and in reverse 
+# 23,,,5, 32
+
+num=input("Enter two digit number")
+#print(type(num))
+n1=num[0]
+n2=num[1]
+#n1=int(num[0])
+#n2=int(num[1])
+print(int(n1)+int(n2))
+#print(n1+n2)
+print(n2+n1)
